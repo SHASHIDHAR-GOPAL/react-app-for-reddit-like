b991e78b932fdbf53f59346c877213db162c5282
SHASHIDHAR-GOPAL <shashidharg01@gmail.com>
2018-07-24 14:15:49 +0530

    Add links to home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import Logo from "./components/logo.js.jsx";
-import Login from "./components/login.js.jsx"
+import Login from "./components/login.js.jsx";
+import Links from "./components/links.js.jsx"
 
 class App extends Component {
   render() {
     return (
-        <div className={"navbar navbar-expand-lg navbar-light bg-light"}>
-          <Logo name="chirp"/>
-          <Login newUser="Sign up" existingUser="Sign in"/>
+        <div>
+            <div className={"navbar navbar-expand-lg navbar-light bg-light"}>
+                <Logo name="chirp"/>
+                <Login newUser="Sign up" existingUser="Sign in"/>
+            </div>
+            <Links message={"Hello"} id={"Joe"}/>
+            <hr className="container"/>
+            <Links message={"What's up?"} id={"ken"}/>
+            <hr className="container"/>
         </div>
     );
   }
